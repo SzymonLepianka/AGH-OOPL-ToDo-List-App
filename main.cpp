3b70f7e44786742ea29660176fa7ac24e0762041
Szymon Lepianka <slepianka@wp.pl>
2022-12-28 21:29:42 +0100

    adds ToDoItem class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+class ToDoItem {
+private:
+    int toDoItemId;
+    string description;
+    bool completed;
+public:
+    ToDoItem(): toDoItemId(0), description(""), completed(false) {
+
+    }
+    ~ToDoItem() = default;
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    list<ToDoItem> toDoItems;
+    toDoItems.clear();
+
+    while (1) {
+        system("cls");
+        cout << "ToDo List App" << endl;
+        cout << endl << endl;
+        
+
+
+    }
+
     return 0;
 }
